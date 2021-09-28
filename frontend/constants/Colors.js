b92ff0624d4ae20531edc2a375c5d5b0ae22dc45
somogyidavid <ksomogyi1515@gmail.com>
2021-09-28 12:43:48 +0200

    feat(colors): added colors constant

---
+++ frontend/constants/Colors.js
@@ -0,0 +1,9 @@
+export default {
+    'light': '#A396E7',
+    'lightPurple': '#6457AC',
+    'darkPurple': '#3F336B',
+    'dark': '#21183C',
+    'green': '#5FBA85',
+    'cyan': '#09C1DE',
+    'magenta': '#EE63B6'
+};
