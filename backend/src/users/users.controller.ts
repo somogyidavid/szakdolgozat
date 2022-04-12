b2aad351bf74f2eb4b4c2cf77cc7f4b7ee21c19b
somogyidavid <ksomogyi1515@gmail.com>
2022-04-12 10:17:20 +0200

    fix: minor bugs fixed

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,47 +1,47 @@
-import { Body, Controller, Delete, Get, Param, Put, UseFilters, UseGuards } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
-import { UsersService } from './users.service';
-import { AuthGuard } from '@nestjs/passport';
-import { UpdateUserDto } from './dto/UpdateUserDto';
-import User from './schemas/user.schema';
-import { ReqUser } from '../auth/decorators/requser.decorator';
-import { Types } from 'mongoose';
-import { DeleteUserDto } from './dto/DeleteUserDto';
-import { ChangePasswordDto } from './dto/ChangePasswordDto';
-import { CastErrorExceptionFilter } from '../exceptions/castError-exception.filter';
-import { ValidationExceptionFilter } from '../exceptions/validation-exception.filter';
-import { ApiErrorExceptionFilter } from '../exceptions/ApiError-exception.filter';
+import { Body, Controller, Delete, Get, Param, Put, UseFilters, UseGuards } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
+import { UsersService } from "./users.service";
+import { AuthGuard } from "@nestjs/passport";
+import { UpdateUserDto } from "./dto/UpdateUserDto";
+import User from "./schemas/user.schema";
+import { ReqUser } from "../auth/decorators/requser.decorator";
+import { Types } from "mongoose";
+import { DeleteUserDto } from "./dto/DeleteUserDto";
+import { ChangePasswordDto } from "./dto/ChangePasswordDto";
+import { CastErrorExceptionFilter } from "../exceptions/castError-exception.filter";
+import { ValidationExceptionFilter } from "../exceptions/validation-exception.filter";
+import { ApiErrorExceptionFilter } from "../exceptions/ApiError-exception.filter";
 
-@ApiTags('Users')
-@Controller('users')
+@ApiTags("Users")
+@Controller("users")
 @UseFilters(new CastErrorExceptionFilter())
 @UseFilters(new ValidationExceptionFilter())
 @UseFilters(new ApiErrorExceptionFilter())
 export class UsersController {
-    constructor(private readonly userService: UsersService) {
-    }
+  constructor(private readonly userService: UsersService) {
+  }
 
-    @UseGuards(AuthGuard('jwt'))
-    @Get('/:userId')
-    async getUserById(@Param('userId') userId: string): Promise<User> {
-        return await this.userService.findById(new Types.ObjectId(userId));
-    }
+  @UseGuards(AuthGuard("jwt"))
+  @Get("/:userId")
+  async getUserById(@Param("userId") userId: string): Promise<User> {
+    return await this.userService.findById(new Types.ObjectId(userId));
+  }
 
-    @UseGuards(AuthGuard('jwt'))
-    @Put('/')
-    async updateUser(@ReqUser('_id') userId: string, @Body() updateUserDto: UpdateUserDto): Promise<User> {
-        return await this.userService.updateUser(new Types.ObjectId(userId), updateUserDto);
-    }
+  @UseGuards(AuthGuard("jwt"))
+  @Put("/")
+  async updateUser(@ReqUser("_id") userId: string, @Body() updateUserDto: UpdateUserDto): Promise<User> {
+    return await this.userService.updateUser(new Types.ObjectId(userId), updateUserDto);
+  }
 
-    @UseGuards(AuthGuard('jwt'))
-    @Delete('/')
-    async deleteUser(@ReqUser('_id') userId: string, @Body() deleteUserDto: DeleteUserDto): Promise<User> {
-        return await this.userService.deleteUser(new Types.ObjectId(userId), deleteUserDto);
-    }
+  @UseGuards(AuthGuard("jwt"))
+  @Delete("/")
+  async deleteUser(@ReqUser("_id") userId: string, @Body() deleteUserDto: DeleteUserDto): Promise<User> {
+    return await this.userService.deleteUser(new Types.ObjectId(userId), deleteUserDto);
+  }
 
-    @UseGuards(AuthGuard('jwt'))
-    @Put('/changePassword')
-    async changePassword(@ReqUser('_id') userId: string, @Body() changePasswordDto: ChangePasswordDto): Promise<User> {
-        return await this.userService.changePassword(new Types.ObjectId(userId), changePasswordDto);
-    }
+  @UseGuards(AuthGuard("jwt"))
+  @Put("/changePassword")
+  async changePassword(@ReqUser("_id") userId: string, @Body() changePasswordDto: ChangePasswordDto): Promise<User> {
+    return await this.userService.changePassword(new Types.ObjectId(userId), changePasswordDto);
+  }
 }
--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,83 +1,85 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { AuthRegisterDto } from '../auth/dto/AuthRegisterDto';
-import { InjectModel } from 'nestjs-typegoose';
-import User from './schemas/user.schema';
-import { ReturnModelType } from '@typegoose/typegoose';
-import { Types } from 'mongoose';
-import { UpdateUserDto } from './dto/UpdateUserDto';
-import { DeleteUserDto } from './dto/DeleteUserDto';
-import * as bcrypt from 'bcryptjs';
-import { ChangePasswordDto } from './dto/ChangePasswordDto';
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { AuthRegisterDto } from "../auth/dto/AuthRegisterDto";
+import { InjectModel } from "nestjs-typegoose";
+import User from "./schemas/user.schema";
+import { ReturnModelType } from "@typegoose/typegoose";
+import { Types } from "mongoose";
+import { UpdateUserDto } from "./dto/UpdateUserDto";
+import { DeleteUserDto } from "./dto/DeleteUserDto";
+import * as bcrypt from "bcryptjs";
+import { ChangePasswordDto } from "./dto/ChangePasswordDto";
 
 @Injectable()
 export class UsersService {
-    constructor(@InjectModel(User) private readonly userModel: ReturnModelType<typeof User>) {
+  constructor(@InjectModel(User) private readonly userModel: ReturnModelType<typeof User>) {
+  }
+
+  async findById(userId: Types.ObjectId): Promise<User> {
+    return await this.userModel.findById(userId).exec();
+  }
+
+  async findByEmail(email: string): Promise<User> {
+    return await this.userModel.findOne({ email }).exec();
+  }
+
+  async registerUser(authRegisterDto: AuthRegisterDto): Promise<User> {
+    let registeredUser = await this.userModel.create({
+      _id: new Types.ObjectId(),
+      email: authRegisterDto.email,
+      password: authRegisterDto.password,
+      name: "",
+      age: 0,
+      description: "",
+      interests: []
+    });
+
+    return await this.userModel.findById(registeredUser._id).select("-password").exec();
+  }
+
+  async updateUser(userId: Types.ObjectId, updateUserDto: UpdateUserDto): Promise<User> {
+    let user = await this.userModel.findById(userId);
+
+    if (!user) {
+      throw new BadRequestException("Nincs ilyen regisztrált felhasználó!");
     }
 
-    async findById(userId: Types.ObjectId): Promise<User> {
-        return await this.userModel.findById(userId).exec();
+    return await this.userModel.findOneAndUpdate({ _id: userId }, updateUserDto, {
+      new: true,
+      context: "query"
+    }).select({ "password": 0 }).exec();
+  }
+
+  async deleteUser(userId: Types.ObjectId, deleteUserDto: DeleteUserDto): Promise<User> {
+    if (deleteUserDto.password !== deleteUserDto.passwordConfirm) {
+      throw new BadRequestException("A jelszavak nem egyeznek meg!");
+    }
+    const user = await this.userModel.findById(userId);
+
+    const correctPassword = await bcrypt.compare(deleteUserDto.password, user.password);
+
+    if (!correctPassword) {
+      throw new BadRequestException("A jelszó hibás!");
     }
 
-    async findByEmail(email: string): Promise<User> {
-        return await this.userModel.findOne({ email }).exec();
+    return await this.userModel.findOneAndRemove({ _id: userId }).select("-password").exec();
+  }
+
+  async changePassword(userId: Types.ObjectId, changePasswordDto: ChangePasswordDto): Promise<User> {
+    if (changePasswordDto.newPassword !== changePasswordDto.newPasswordConfirm) {
+      throw new BadRequestException("A jelszavak nem egyeznek meg!");
+    }
+    const user = await this.userModel.findById(userId);
+
+    const correctPassword = await bcrypt.compare(changePasswordDto.password, user.password);
+
+    if (!correctPassword) {
+      throw new BadRequestException("A jelszó hibás!");
     }
 
-    async registerUser(authRegisterDto: AuthRegisterDto): Promise<User> {
-        let registeredUser = await this.userModel.create({
-            _id: new Types.ObjectId(),
-            email: authRegisterDto.email,
-            password: authRegisterDto.password,
-            name: '',
-            age: 0,
-            description: '',
-            interests: []
-        });
-
-        return await this.userModel.findById(registeredUser._id).select('-password').exec();
-    }
-
-    async updateUser(userId: Types.ObjectId, updateUserDto: UpdateUserDto): Promise<User> {
-        let user = await this.userModel.findById(userId);
-
-        if (!user) {
-            throw new BadRequestException('Nincs ilyen regisztrált felhasználó!');
-        }
-
-        return await this.userModel.findOneAndUpdate({ _id: userId }, updateUserDto, {
-            new: true,
-            context: 'query'
-        }).select({ 'password': 0 }).exec();
-    }
-
-    async deleteUser(userId: Types.ObjectId, deleteUserDto: DeleteUserDto): Promise<User> {
-        if (deleteUserDto.password !== deleteUserDto.passwordConfirm) {
-            throw new BadRequestException('A jelszavak nem egyeznek meg!');
-        }
-        const user = await this.userModel.findById(userId);
-
-        const correctPassword = await bcrypt.compare(deleteUserDto.password, user.password);
-
-        if (!correctPassword) {
-            throw new BadRequestException('A jelszó hibás!');
-        }
-
-        return await this.userModel.findOneAndRemove({ _id: userId }).select('-password').exec();
-    }
-
-    async changePassword(userId: Types.ObjectId, changePasswordDto: ChangePasswordDto): Promise<User> {
-        if (changePasswordDto.newPassword !== changePasswordDto.newPasswordConfirm) {
-            throw new BadRequestException('A jelszavak nem egyeznek meg!');
-        }
-        const user = await this.userModel.findById(userId);
-
-        const correctPassword = await bcrypt.compare(changePasswordDto.password, user.password);
-
-        if (!correctPassword) {
-            throw new BadRequestException('A jelszó hibás!');
-        }
-
-        user.password = changePasswordDto.newPassword;
-        await user.save();
-        return user;
-    }
+    user.password = bcrypt.hashSync(changePasswordDto.newPassword, 10);
+    return await this.userModel.findOneAndUpdate({ _id: userId }, user, {
+      new: true,
+      context: "query"
+    }).select({ "password": 0 }).exec();
+  }
 }
